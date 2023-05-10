284485a2e13eb1c7e84f61778a5c08d2abb2fddf
ABCorp <116996967+ABCorpDark@users.noreply.github.com>
2023-05-10 08:13:40 +0530

    Create time_dependence.sol

---
+++ time_dependence.sol
@@ -0,0 +1,21 @@
+#TIME DEPENDENCY ATTACK:
+
+pragma solidity ^0.8.0;
+
+contract TimeDependentContract {
+    uint public deadline;
+    uint public reward = 10 ether;
+    address payable public winner;
+
+    constructor(uint _deadline) {
+        deadline = _deadline;
+    }
+
+    function claimReward() public {
+        require(block.timestamp >= deadline, "Reward not available yet");
+        require(msg.sender != winner, "Reward already claimed");
+        winner = payable(msg.sender);
+        winner.transfer(reward);
+        reward = 0;
+    }
+}
