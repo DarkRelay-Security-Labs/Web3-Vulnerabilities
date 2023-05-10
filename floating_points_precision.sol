b107ae4010c53ee2aee0ee30fde1e909aabd7e32
ABCorp <116996967+ABCorpDark@users.noreply.github.com>
2023-05-10 08:15:25 +0530

    Create floating_points_precision.sol

---
+++ floating_points_precision.sol
@@ -0,0 +1,19 @@
+#FLOATING AND PRECISION ATTACK: 
+
+contract TokenSale {
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    function buyTokens(uint256 _amount) public payable {
+        uint256 cost = _amount * tokenPrice;
+        require(msg.value == cost);
+        tokensSold += _amount;
+    }
+
+    function sellTokens(uint256 _amount) public {
+        uint256 payout = _amount * tokenPrice;
+        require(address(this).balance >= payout);
+        tokensSold -= _amount;
+        msg.sender.transfer(payout);
+    }
+}
